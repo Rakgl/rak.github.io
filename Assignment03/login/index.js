4ce90fba599f45afd465d9e2594db90b7af8c9e9
Kosal linnorak <rak@Kosals-Laptop.local>
2024-05-21 13:14:57 +0700

    assignment3

---
+++ Assignment03/login/index.js
@@ -0,0 +1,19 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    goToPage();
+});
+
+function goToPage(){
+    var username = document.getElementById("username").value;
+    var pwd = document.getElementById("pwd").value;
+
+    var correctUsername = "rak";
+    var correctPassword = "123";
+    if (username === correctUsername && pwd === correctPassword) {
+        window.location.href = 'http://127.0.0.1:5500/Product/index.html';
+    }
+    else{
+        alert("Invalid username or password. Please try again.");
+    }
+    
+}
